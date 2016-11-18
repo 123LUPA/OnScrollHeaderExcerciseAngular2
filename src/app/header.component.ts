ab980e8fc25ab12ced31fc11b81c1e18607d98bc
Klucinka007 <Palkovicova.lucia@gmail.com>
2016-11-18 11:12:17 +0100

    Exercise done

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,3 +1,44 @@
+import {Component} from "@angular/core";
 /**
  * Created by Palko on 17/11/2016.
  */
+
+@Component({
+  selector: 'headerScroll',
+  host: {'(window:scroll)': 'track($event)'},
+
+  templateUrl: './header.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class HeaderComponent {
+
+  currPos: number = 0;
+  nextPos: number = 0;
+  hideElement = false;
+  i:number =0;
+
+  track():void {
+
+    this.currPos = (window.pageYOffset);
+
+    //displays the header at the beginning
+    if(this.i<1){
+      this.hideElement = false;
+      this.i++;
+    }
+
+    //hides the header when scrolling down
+    else if (this.nextPos <this.currPos) {
+      this.nextPos = this.currPos;
+      this.hideElement = true;
+    }
+
+    //displays the header when scrolling up
+    else {
+      this.nextPos > this.currPos;
+      this.nextPos = this.currPos;
+      this.hideElement = false;
+    }
+
+  }
+}
